db106273f3930f8134122525bb0bb47cd5024bcf
Sean Shmulevich <seanshmulevich@Seans-MBP-3.fritz.box>
2023-08-01 00:51:49 +0200

    Hello world

--- src/routes/(user-pages)/matches/+page.svelte
+++ src/routes/(user-pages)/matches/+page.svelte
@@ -56,85 +56,90 @@
 		</div>
 	</div>
 {/if}
-<div
-	class="flex flex-row -mt-5 w-[80%] sm:w-[60%] mb-5 text-white text-center justify-center mx-auto"
->
-	<button
-		on:click|preventDefault={() => {
-			activeButton = 'new';
-			interestedBrands = newDeals;
-		}}
-		class=" {activeButton === 'new'
-			? 'border-4 border-green-400'
-			: ''} bg-gray-800 w-1/3 p-3 rounded-xl text-center mt-10 text-lg">New</button
-	>
-	<button
-		on:click|preventDefault={() => {
-			activeButton = 'ongoing';
-			interestedBrands = ongoingDeals;
-		}}
-		class=" {activeButton === 'ongoing'
-			? 'border-4 border-green-400'
-			: ''} bg-gray-800 w-1/3 p-3 rounded-xl text-center mt-10 text-lg">Ongoing</button
-	>
-	<button
-		on:click|preventDefault={() => {
-			// currentDealList = completedDeals;
-			activeButton = 'completed';
-			interestedBrands = completedDeals;
-		}}
-		class="  {activeButton === 'completed'
-			? 'border-4 border-green-400'
-			: ''} bg-gray-800 w-1/3 p-3 rounded-xl text-center mt-10 text-lg">Completed</button
-	>
-</div>
-{#each interestedBrands as currDeal}
-	<div class="w-fit mx-auto bg-gray-800 rounded-xl mb-5 p-5">
-		<h2 class="text-center text-3xl mb-2 text-white">{currDeal.title}</h2>
-		<hr />
-		<div class="flex flex-col md:flex-row">
-			{#if currDeal.dealImages.length !== 0}
-				<img
-					class="mt-5 rounded-2xl mx-auto sm:mx-[unset] h-[500px] w-[400px]"
-					src="/api/s3object/{currDeal.dealImages[0].id}"
-				/>
-			{:else}
-				<img class="mt-5 rounded-2xl mx-auto sm:mx-[unset] h-[500px] w-[400px]" src={defaultImg} />
-			{/if}
-			<div class="text-white mt-5 w-full md:w-[25%] min-w-[300px]">
-				<div class="border border-white p-5 text-lg rounded-xl">
-					<p>Event Type</p>
-					<p class="mb-5">{currDeal.eventType}</p>
-					<p>Sport Preference</p>
-					<p class="mb-5">{currDeal.sportPreference}</p>
-					<p>Gender Preference:</p>
-					<p>{currDeal.genderPreference}</p>
-				</div>
-				<!-- <p>{currDeal.shortDescription}</p> -->
-				<!-- <p>{currDeal.endDate}</p> -->
-				<!-- <p>{currDeal.publishDate}</p> -->
-				<!-- <p>{currDeal.isCampaign}</p> -->
-				<!-- <p>{currDeal.singleOrMultiple}</p> -->
-				<!-- <p>{currDeal.athleteCount}</p> -->
-				<!-- <p>{currDeal.}</p> -->
 
-				<div class="border border-white p-5 text-lg rounded-xl">
-					<p>Location</p>
-					{#if currDeal.location !== ''}
-						<p class="mb-5">{currDeal.location}</p>
-					{:else}
-						<p class="mb-5">{currDeal.inPersonOrVirtual}</p>
-					{/if}
+<div class="overflow-y-auto mt-32">
+	<div
+		class="flex flex-row -mt-5 w-[80%] sm:w-[60%] mb-5 text-white text-center justify-center mx-auto"
+	>
+		<button
+			on:click|preventDefault={() => {
+				activeButton = 'new';
+				interestedBrands = newDeals;
+			}}
+			class=" {activeButton === 'new'
+				? 'border-4 border-green-400'
+				: ''} bg-gray-800 w-1/3 p-3 rounded-xl text-center mt-10 text-lg">New</button
+		>
+		<button
+			on:click|preventDefault={() => {
+				activeButton = 'ongoing';
+				interestedBrands = ongoingDeals;
+			}}
+			class=" {activeButton === 'ongoing'
+				? 'border-4 border-green-400'
+				: ''} bg-gray-800 w-1/3 p-3 rounded-xl text-center mt-10 text-lg">Ongoing</button
+		>
+		<button
+			on:click|preventDefault={() => {
+				// currentDealList = completedDeals;
+				activeButton = 'completed';
+				interestedBrands = completedDeals;
+			}}
+			class="  {activeButton === 'completed'
+				? 'border-4 border-green-400'
+				: ''} bg-gray-800 w-1/3 p-3 rounded-xl text-center mt-10 text-lg">Completed</button
+		>
+	</div>
+	{#each interestedBrands as currDeal}
+		<div class="w-fit mx-auto bg-gray-800 rounded-xl mb-5 p-5">
+			<h2 class="text-center text-3xl mb-2 text-white">{currDeal.title}</h2>
+			<hr />
+			<div class="flex flex-col md:flex-row">
+				{#if currDeal.dealImages.length !== 0}
+					<img
+						class="mt-5 rounded-2xl mx-auto sm:mx-[unset] h-[500px] w-[400px]"
+						src="/api/s3object/{currDeal.dealImages[0].id}"
+					/>
+				{:else}
+					<img
+						class="mt-5 rounded-2xl mx-auto sm:mx-[unset] h-[500px] w-[400px]"
+						src={defaultImg}
+					/>
+				{/if}
+				<div class="text-white mt-5 w-full md:w-[25%] min-w-[300px]">
+					<div class="border border-white p-5 text-lg rounded-xl">
+						<p>Event Type</p>
+						<p class="mb-5">{currDeal.eventType}</p>
+						<p>Sport Preference</p>
+						<p class="mb-5">{currDeal.sportPreference}</p>
+						<p>Gender Preference:</p>
+						<p>{currDeal.genderPreference}</p>
+					</div>
+					<!-- <p>{currDeal.shortDescription}</p> -->
+					<!-- <p>{currDeal.endDate}</p> -->
+					<!-- <p>{currDeal.publishDate}</p> -->
+					<!-- <p>{currDeal.isCampaign}</p> -->
+					<!-- <p>{currDeal.singleOrMultiple}</p> -->
+					<!-- <p>{currDeal.athleteCount}</p> -->
+					<!-- <p>{currDeal.}</p> -->
 
-					<p class="mb-5">
-						{#if currDeal.isCampaign}
-							Start Date: {currDeal.eventDate.toISOString().slice(0, 10)}
-							End Date: {currDeal.endDate.toISOString().slice(0, 10)}
+					<div class="border border-white p-5 text-lg rounded-xl">
+						<p>Location</p>
+						{#if currDeal.location !== ''}
+							<p class="mb-5">{currDeal.location}</p>
 						{:else}
-							Date: {currDeal.eventDate.toISOString().slice(0, 10)}
+							<p class="mb-5">{currDeal.inPersonOrVirtual}</p>
 						{/if}
-					</p>
-					<!-- <p>End Date</p>
+
+						<p class="mb-5">
+							{#if currDeal.isCampaign}
+								Start Date: {currDeal.eventDate.toISOString().slice(0, 10)}
+								End Date: {currDeal.endDate.toISOString().slice(0, 10)}
+							{:else}
+								Date: {currDeal.eventDate.toISOString().slice(0, 10)}
+							{/if}
+						</p>
+						<!-- <p>End Date</p>
 					<p class="mb-5">
 						{new Date(currDeal.endDate).toLocaleDateString('en-GB', {
 							day: 'numeric',
@@ -142,88 +147,89 @@
 							year: 'numeric'
 						})}
 					</p> -->
-					<p class="mt-5 underline">Compensation</p>
-					{#if JSON.parse(currDeal.estimatedPayment)['pay'] === 'Both'}
-						<p>Product: {JSON.parse(currDeal.estimatedPayment).product}</p>
-						<p>Money: {JSON.parse(currDeal.estimatedPayment).compSelected}</p>
-					{:else if JSON.parse(currDeal.estimatedPayment)['pay'] === 'Money'}
-						<p>Money: {JSON.parse(currDeal.estimatedPayment).compSelected}</p>
-					{:else if JSON.parse(currDeal.estimatedPayment)['pay'] === 'Product'}
-						<p>Product {JSON.parse(currDeal.estimatedPayment).product}</p>
-					{/if}
+						<p class="mt-5 underline">Compensation</p>
+						{#if JSON.parse(currDeal.estimatedPayment)['pay'] === 'Both'}
+							<p>Product: {JSON.parse(currDeal.estimatedPayment).product}</p>
+							<p>Money: {JSON.parse(currDeal.estimatedPayment).compSelected}</p>
+						{:else if JSON.parse(currDeal.estimatedPayment)['pay'] === 'Money'}
+							<p>Money: {JSON.parse(currDeal.estimatedPayment).compSelected}</p>
+						{:else if JSON.parse(currDeal.estimatedPayment)['pay'] === 'Product'}
+							<p>Product {JSON.parse(currDeal.estimatedPayment).product}</p>
+						{/if}
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="w-full -mt-5 py-5">
-			<div class="border border-1 rounded-xl text-white p-5">
-				<p class="text-xl">Deliverables</p>
-				<hr class="mb-2" />
-				<ol>
-					{#each currDeal.recommendedDeliverables['set'] as del}
-						<li class="m-5">
-							{del.title}: {del.value}
-						</li>
-					{/each}
-				</ol>
-			</div>
-			<!-- <div class="border border-1 rounded-xl text-white p-5">
+			<div class="w-full -mt-5 py-5">
+				<div class="border border-1 rounded-xl text-white p-5">
+					<p class="text-xl">Deliverables</p>
+					<hr class="mb-2" />
+					<ol>
+						{#each currDeal.recommendedDeliverables['set'] as del}
+							<li class="m-5">
+								{del.title}: {del.value}
+							</li>
+						{/each}
+					</ol>
+				</div>
+				<!-- <div class="border border-1 rounded-xl text-white p-5">
 				<p class="text-xl">Goals</p>
 				<hr class="mb-2" />
 				{#each currDeal.goals as goal}
 					<p>{goal}</p>
 				{/each}
 			</div> -->
-		</div>
-		<div class="text-center text-white mx-auto flex flex-row">
-			{#if activeButton !== 'completed'}
-				<button
-					on:click={() => {
-						showModal = true;
-						brandEmail = currDeal.authUser.email;
-					}}
-					class="p-3i border border-white bg-gray-700 w-1/3 rounded-xl"
-				>
-					Contact Brand
-				</button>
-				<a
-					href="/brand-profile/{currDeal.authUserId}"
-					class="p-3 border border-white bg-gray-700 w-1/3 rounded-xl"
-				>
-					View Profile
-				</a>
-				{#if activeButton === 'new'}
-					<form
-						class="w-1/3 border bg-gray-700 rounded-xl border-white"
-						method="POST"
-						use:enhance
-						action="?/agree"
+			</div>
+			<div class="text-center text-white mx-auto flex flex-row">
+				{#if activeButton !== 'completed'}
+					<button
+						on:click={() => {
+							showModal = true;
+							brandEmail = currDeal.authUser.email;
+						}}
+						class="p-3i border border-white bg-gray-700 w-1/3 rounded-xl"
 					>
-						<button
-							on:click={() => {
-								delId = currDeal.id;
-								loading = true;
-							}}
-							name="agree-deal"
-							value={currDeal.id}
-							type="submit"
-							class="w-full h-full"
+						Contact Brand
+					</button>
+					<a
+						href="/brand-profile/{currDeal.authUserId}"
+						class="p-3 border border-white bg-gray-700 w-1/3 rounded-xl"
+					>
+						View Profile
+					</a>
+					{#if activeButton === 'new'}
+						<form
+							class="w-1/3 border bg-gray-700 rounded-xl border-white"
+							method="POST"
+							use:enhance
+							action="?/agree"
 						>
-							Agree to offer
+							<button
+								on:click={() => {
+									delId = currDeal.id;
+									loading = true;
+								}}
+								name="agree-deal"
+								value={currDeal.id}
+								type="submit"
+								class="w-full h-full"
+							>
+								Agree to offer
+							</button>
+						</form>
+					{/if}
+					{#if activeButton === 'ongoing'}
+						<button class="p-3 border border-white bg-gray-700 w-1/3 rounded-xl">
+							View Contract
 						</button>
-					</form>
-				{/if}
-				{#if activeButton === 'ongoing'}
-					<button class="p-3 border border-white bg-gray-700 w-1/3 rounded-xl">
+					{/if}
+				{:else}
+					<button class="p-3 border mx-auto border-white bg-gray-700 w-1/3 rounded-xl">
 						View Contract
 					</button>
 				{/if}
-			{:else}
-				<button class="p-3 border mx-auto border-white bg-gray-700 w-1/3 rounded-xl">
-					View Contract
-				</button>
-			{/if}
+			</div>
 		</div>
-	</div>
-{:else}
-	<p class=" text-3xl text-red-500 text-center">Sorry no deals here :(</p>
-{/each}
+	{:else}
+		<p class=" text-3xl text-red-500 text-center">Sorry no deals here :(</p>
+	{/each}
+</div>

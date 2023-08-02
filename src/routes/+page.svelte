01a3b625b22cef3bde76baa32fa357e111aaabbd
Sean Shmulevich <seanshmulevich@Seans-MBP-3.fritz.box>
2023-08-03 00:14:02 +0200

    clean mobile home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 		</div>
 	</div>
 	<img
-		class="sm:hidden block mx-auto -mt-20 -mb-8"
+		class="sm:hidden block mx-auto -mt-[110px] -mb-0"
 		src="https://shmul.dev/assets/dapupmainimg.png"
 	/>
 	<a

21b4f94535c12ebebcd4adcec350d79a4d56d7ef
Sean Shmulevich <seanshmulevich@Seans-MBP-3.fritz.box>
2023-08-03 02:31:00 +0200

    typo

--- src/routes/coming-soon/+page.svelte
+++ src/routes/coming-soon/+page.svelte
@@ -1,4 +1,4 @@
 <h1 class="text-3xl text-center mt-14 text-red-500">
-	Brand portal is in private acess mode, interested brands contact admin@dapup.co in order to
-	request acess
+	Brand portal is in private access mode, interested brands contact admin@dapup.co in order to
+	request access
 </h1>

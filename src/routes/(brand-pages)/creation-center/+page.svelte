f415678aee95260635b8dbb89ef20f0e34233b52
Agpancho360 <101365646+Agpancho360@users.noreply.github.com>
2024-01-31 12:22:43 -0600

    creation center UI improvements

--- src/routes/(brand-pages)/creation-center/+page.svelte
+++ src/routes/(brand-pages)/creation-center/+page.svelte
@@ -185,14 +185,14 @@
 	</div>
 	{#each currentDealList as deal}
 		<div
-			class="bg-gray-700 text-white rounded-xl mb-10 w-[80%] lg:w-[40%] flex flex-col shadow-md p-5 mx-auto"
+			class="bg-gray-700 text-white rounded-xl w-[85%] lg:w-[40%] flex flex-col sm:flex-row shadow-md p-5 mx-auto my-5"
 		>
-			<div class="bg-gray-200 w-full sm:w-[60%] mx-auto">
+			<div class="bg-gray-200 w-full sm:w-[60%] mx-auto rounded-lg overflow-hidden">
 				{#if deal && deal.dealImages && deal.dealImages.length !== 0}
 					<img
 						src="/api/s3object/{deal.dealImages[0].id}"
 						alt="Brand deal to athlete {deal.title}"
-						class="w-full h-full object-cover"
+						class="w-full h-full aspect-square object-cover object-center"
 					/>
 				{:else}
 					<img
@@ -203,45 +203,78 @@
 				{/if}
 				<!-- You need to implement the Image component yourself -->
 			</div>
-			<div class="p-8 w-full flex flex-row">
+			<div class=" py-4 w-full flex flex-row">
 				<div class="w-full">
-					<h1 class="font-bold text-xl mb-2">{deal.title}</h1>
-					{#if deal.isCampaign}
+					<div class="flex justify-between items-center">
+						<h1 class="font-bold text-2xl mb-2 text-center uppercase">{deal.title}</h1>
+						<div class="text-sm text-gray-400 ">
+							<!-- Published: {deal.publishDate.toISOString().slice(0, 10)} -->
+							2 days ago
+						</div>
+					</div>
+					<!-- {#if deal.isCampaign}
 						<p class="font-bold">Campaign</p>
 					{:else}
 						<p class="font-bold">Single Event</p>
-					{/if}
-					<p class="text-white break-words text-base">Description: {deal.shortDescription}</p>
-					<div class="mt-4">
-						<div class="flex items-center">
+					{/if} -->
+					<div class="flex text-xs justify-between mt-2 mb-4">
+						<p class="rounded-lg border-2 p-1 border-green-500 font-bold text-green-500 ">{deal.sportPreference}</p>
+						<p class="rounded-lg border-2 p-1 border-green-500 font-bold text-green-500">{deal.genderPreference}</p>
+						<p class="rounded-lg border-2 p-1 border-green-500 font-bold text-green-500">{JSON.parse(deal.estimatedPayment).compSelected}</p>
+						<p class="rounded-lg border-2 p-1 border-green-500 font-bold text-green-500">{deal.athleteCount} Athlete</p>
+					</div>
+					<!-- <p class="text-sm text-center rounded-lg mb-4 border-2 py-1">{deal.sportPreference} | {deal.genderPreference} | {JSON.parse(deal.estimatedPayment).compSelected} | {deal.athleteCount} Athelete</p> -->
+					<p class="text-sm text-white break-words ">
+						<span class="font-bold">Description: </span>
+							<!-- {deal.shortDescription} -->
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nesciunt iure reiciendis at eaque? Debitis incidunt vel repellat! Aliquam quam asperiores, modi quo consequatur doloribus reprehenderit error debitis sapiente deserunt.
+						
+					</p>
+			
+					<div class="mt-7">
+						<!-- <div class="flex items-center">
 							<div class="text-sm text-white">Sport Preference: {deal.sportPreference}</div>
 							<div class="ml-3 text-sm text-white">Gender Preference: {deal.genderPreference}</div>
-						</div>
-						<div class="mt-2 flex items-center">
+						</div> -->
+						<div class="flex justify-between my-2">
+							<div class="flex items-center">
 							{#if !deal.isCampaign}
 								{#if deal.location !== ''}
-									<div class="text-sm text-white">Location: {deal.location}</div>
+									<div class="text-sm text-white">Location {deal.location}</div>
 								{:else}
-									<div class="text-sm text-white">Location: {deal.inPersonOrVirtual}</div>
+									<div class=" text-white">
+										<svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-gray-400 mr-1" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+										</div>
+									<span class="text-xs text-gray-400">{deal.inPersonOrVirtual}</span>
 								{/if}
 							{/if}
 						</div>
 
 						{#if !deal.isCampaign}
+						<div class="flex items-center">
 							<div class="text-sm text-white">
-								Event type: {deal.eventType !== '' ? deal.eventType : 'no event type'}
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-gray-400 mr-1" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM72 272a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zm104-16H304c8.8 0 16 7.2 16 16s-7.2 16-16 16H176c-8.8 0-16-7.2-16-16s7.2-16 16-16zM72 368a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zm88 0c0-8.8 7.2-16 16-16H304c8.8 0 16 7.2 16 16s-7.2 16-16 16H176c-8.8 0-16-7.2-16-16z"/></svg>							
 							</div>
-						{/if}
-						<div class="text-sm text-white">
-							{#if deal.isCampaign}
-								Start Date: {deal.eventDate.toISOString().slice(0, 10)}
-								End Date: {deal.endDate.toISOString().slice(0, 10)}
-							{:else}
-								Date: {deal.eventDate.toISOString().slice(0, 10)}
-							{/if}
+							<span class="text-xs text-gray-400">{deal.eventType !== '' ? deal.eventType : 'no event type'}</span>
 						</div>
+						{/if}
+						<div class="flex items-center">
+							<div class="text-sm ">
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-gray-400 mr-1" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z"/></svg>
+							
+							</div>
+							<div class="text-xs text-gray-400 ">
+								{#if deal.isCampaign}
+									{deal.eventDate.toISOString().slice(0, 10)}-{deal.endDate.toISOString().slice(0, 10)}
+								{:else}
+									{deal.eventDate.toISOString().slice(0, 10)}
+								{/if}
+							</div>
+						</div></div>
+						
+						
 
-						<p class="mt-5 underline">Compensation</p>
+						<!-- <p class="mt-5 underline">Compensation</p>
 						{#if JSON.parse(deal.estimatedPayment)['pay'] === 'Both'}
 							{#if JSON.parse(deal.estimatedPayment).product !== ''}
 								<p>Product: {JSON.parse(deal.estimatedPayment).product}</p>
@@ -261,18 +294,14 @@
 							{#if JSON.parse(deal.estimatedPayment).product !== ''}
 								<p>Product: {JSON.parse(deal.estimatedPayment).product}</p>
 							{/if}
-						{/if}
-						<div class="text-sm text-white mt-5">Athletes: {deal.athleteCount}</div>
-						<div class="flex items-center">
-							<div class="text-sm text-white">
-								Published: {deal.publishDate.toISOString().slice(0, 10)}
-							</div>
-						</div>
+						{/if} -->
+						<!-- <div class="text-sm text-white mt-5">Athletes: {deal.athleteCount}</div> -->
+					
 					</div>
-					<div class="w-full text-center">
+					<div class="w-full text-center hidden"><!--added hidden-->
 						<a class="mx-auto" href="{dealLink}{deal.id}">
 							<button
-								class="bg-green-500 hover:bg-green-700 text-white transform translate-y-5 font-bold p-2 rounded-full"
+								class="bg-green-500 hover:bg-green-700 text-white  transform translate-y-5 font-bold p-2 rounded-full"
 							>
 								{#if activeButton === 'unpublished'}
 									Finish creating deal
@@ -287,7 +316,7 @@
 				</div>
 			</div>
 			{#if activeButton !== 'completed' && activeButton !== 'unpublished'}
-				<button
+				<!-- <button
 					on:click|preventDefault={() => {
 						showModalComplete = true;
 						nah = false;
@@ -297,7 +326,23 @@
 					class="bg-yellow-300 text-black p-2 rounded-full mr-auto transform -translate-y-3"
 				>
 					End offer
-				</button>
+				</button> -->
+				<div class="mt-2 flex flex-row justify-between">
+
+				
+				<button
+						class="border-2 border-green-500 hover:bg-green-700 text-green-500 font-bold p-2 rounded-full uppercase text-sm"
+					>
+					<a class="mx-auto" href="{dealLink}{deal.id}">
+						{#if activeButton === 'unpublished'}
+							Finish creating deal
+						{:else if activeButton === 'completed'}
+							View Details
+						{:else if activeButton === 'published'}
+							See user activity
+						{/if}
+					</a>
+					</button>
 				<button
 					on:click|preventDefault={() => {
 						showModal = true;
@@ -305,10 +350,11 @@
 						deleteName = deal.title;
 						deleteId = deal.id;
 					}}
-					class="ml-auto -mt-14 mr-0 w-fit"
+					class=""
 				>
-					<img class="w-10" alt="delete deal" src="https://slimecars.com/assets/trash.png" />
+					<img class="w-8" alt="delete deal" src="https://slimecars.com/assets/trash.png" />
 				</button>
+			</div>
 			{/if}
 		</div>
 	{:else}

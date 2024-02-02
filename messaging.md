24ee0993bdb070f82a4131f5b8b03e97d0a42fd1
Sean Shmulevich <shmul@Seans-MacBook-Pro-2.local>
2024-02-02 00:55:26 -0500

    supabase?

---
+++ messaging.md
@@ -0,0 +1,6 @@
+How will messaging work.
+
+We will need two databases because our current host cannot maintain a web-socket connection
+
+-   How do we query the database
+    -   The contract or messaging page can currently be found by the deal-id and the athlete-id.
--- src/routes/(user-pages)/deals/view-contract/+page.server.js
+++ src/routes/(user-pages)/deals/view-contract/+page.server.js
@@ -1,7 +1,36 @@
 import { prismaClient } from '$lib/db';
 import { fail, redirect } from '@sveltejs/kit';
 import { auth } from '$lib/lucia';
+// src/hooks.server.ts
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 
+// After an action runs, the page will be re-rendered (unless a redirect or an unexpected error occurs), with the action's return value available to the page as the form prop. This means that your page's load functions will run after the action completes.
+
+// Note that handle runs before the action is invoked, and does not rerun before the load functions. This means that if, for example, you use handle to populate event.locals based on a cookie, you must update event.locals when you set or delete the cookie in an action:
+export const handle = async ({ event, resolve }) => {
+	event.locals.supabase = createSupabaseServerClient({
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		event
+	});
+
+	/**
+	 * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
+	 */
+	event.locals.getSession = async () => {
+		const {
+			data: { session }
+		} = await event.locals.supabase.auth.getSession();
+		return session;
+	};
+
+	return resolve(event, {
+		filterSerializedResponseHeaders(name) {
+			return name === 'content-range';
+		}
+	});
+};
 export const load = async ({ params, locals, url }) => {
 	const { user } = await locals.auth.validateUser();
 	// get userID and deal ID params
